33d5e8cd9649bf588762fd4308107b39646da300
Akshat-shah05 <akshatshah1605@gmail.com>
2023-08-11 19:37:41 -0400

    winner useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { nanoid } from 'nanoid'
 import './App.css'
 import Dice from './components/Dice'
@@ -8,15 +8,23 @@ function App() {
   // Initialize dice value state, on render, allNewDice() is called
   const [dice, setDice] = useState(allNewDice())
 
+  // Winner State
+  const [tenzies, setTenzies] = useState(false)
+
+  // Helper function for reusability
+  function generateNewDice() {
+    return {
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: false, 
+        id: nanoid()
+    }
+  }
+
   // Function to randomly generate the dice numbers
   function allNewDice() {
     let newDice = []
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6), 
-        isHeld: false, 
-        id: nanoid()
-      })
+      newDice.push(generateNewDice())
     }
     return newDice
   }
@@ -31,24 +39,37 @@ function App() {
 
   // Function to roll the dice (setDice again) onClick
   function rollDice() {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ? {...die} : generateNewDice()
+    }))
   }
 
+  // Function to hold the dice when it is clicked
   function holdDice(id) {
     setDice(oldDice => oldDice.map(die => {
         return die.id === id ? {...die, isHeld: !die.isHeld} : die
       }))
     }
-  
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)  
+    const val = dice[0].value
+    const sameValue = dice.every(die => die.value === val)
+    if (allHeld && sameValue) {
+      setTenzies(true)
+      console.log("winner")
+    }
+  }, [dice])
 
   return (
     <>
       <h1 className="title"> Welcome to Tenzies! </h1>
+      <h4 className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h4>
       <main>
         <div className="dice--container">
           {diceElements}
         </div>
-        <button onClick={rollDice} className="roll-dice"> Roll </button>
+        <button onClick={rollDice} className="roll-dice"> {tenzies ? "Roll" : "New Game"} </button>
       </main>
     </>
   )
